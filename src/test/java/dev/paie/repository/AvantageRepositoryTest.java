1750c6159360fa973e39b4558eb1f9df624af58d
Anthony Frugier <fru.anthony@gmail.com>
2017-08-10 11:52:40 +0200

    Mise en place de Spring MVC

--- src/test/java/dev/paie/repository/AvantageRepositoryTest.java
+++ src/test/java/dev/paie/repository/AvantageRepositoryTest.java
@@ -26,20 +26,14 @@ public class AvantageRepositoryTest {
 		Avantage newAvantage = new Avantage("PR", "Prime Annuelle", new BigDecimal(150));
 		Avantage avantageMAJ = new Avantage("PR", "Prime Mensuelle", new BigDecimal(100));
 
-		// TODO sauvegarder un nouvel avantage
 		avantageRepository.save(newAvantage);
-		// TODO vérifier qu'il est possible de récupérer le nouvel avantage via
-		// la méthode findOne
 		assertThat(avantageRepository.findOne(newAvantage.getId()).equals(newAvantage));
-		// TODO modifier un avantage
 		Avantage avantageAMAJ = avantageRepository.findOne(newAvantage.getId());
 		avantageAMAJ.setId(avantageMAJ.getId());
 		avantageAMAJ.setCode(avantageMAJ.getCode());
 		avantageAMAJ.setNom(avantageMAJ.getNom());
 		avantageAMAJ.setMontant(avantageMAJ.getMontant());
 		avantageRepository.save(avantageAMAJ);
-		// TODO vérifier que les modifications sont bien prises en compte via la
-		// méthode findOne
 		assertThat(avantageRepository.findOne(avantageAMAJ.getId()).equals(avantageMAJ));
 	}
 
--- src/test/java/dev/paie/service/GradeServiceJdbcTemplateTest.java
+++ src/test/java/dev/paie/service/GradeServiceJdbcTemplateTest.java
@@ -4,12 +4,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.math.BigDecimal;
 
-import javax.sql.DataSource;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -22,19 +19,9 @@ public class GradeServiceJdbcTemplateTest {
 
 	@Autowired
 	private GradeService gradeService;
-	@Autowired
-	private DataSource dataSource;
-	private JdbcTemplate jdbcTemplate;
-
-	// @Before
-	// public void init() {
-	// this.jdbcTemplate = new JdbcTemplate(dataSource);
-	// String sql = "CREATE TABLE IF NOT EXISTS `grade` (`id` int(11)
-	// AUTO_INCREMENT, `code` VARCHAR(255),"
-	// + "`nbHeuresBases` DECIMAL(10,0), `tauxBase` DECIMAL(10,0), PRIMARY KEY
-	// (`id`))";
-	// this.jdbcTemplate.execute(sql);
-	// }
+	// @Autowired
+	// private DataSource dataSource;
+	// private JdbcTemplate jdbcTemplate;
 
 	@Test
 	public void test_sauvegarder_lister_mettre_a_jour() {
