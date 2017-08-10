8ac0aa99bbc770590b5e6da6d353d2987a56b4b5
Anthony Frugier <fru.anthony@gmail.com>
2017-08-10 16:19:53 +0200

    Génération du jeu d'éssaie avec la période

--- src/main/java/dev/paie/service/InitialiserDonneesServiceDev.java
+++ src/main/java/dev/paie/service/InitialiserDonneesServiceDev.java
@@ -1,6 +1,9 @@
 package dev.paie.service;
 
+import java.time.LocalDate;
+import java.time.temporal.TemporalAdjusters;
 import java.util.Map;
+import java.util.stream.IntStream;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
@@ -9,10 +12,12 @@ import org.springframework.stereotype.Service;
 import dev.paie.entite.Cotisation;
 import dev.paie.entite.Entreprise;
 import dev.paie.entite.Grade;
+import dev.paie.entite.Periode;
 import dev.paie.entite.ProfilRemuneration;
 import dev.paie.repository.CotisationRepository;
 import dev.paie.repository.EntreprisesRepository;
 import dev.paie.repository.GradeRepository;
+import dev.paie.repository.PeriodeRepository;
 import dev.paie.repository.ProfilRemunerationRepository;
 
 @Service
@@ -28,6 +33,8 @@ public class InitialiserDonneesServiceDev implements InitialiserDonneesService {
 	GradeRepository repoGrade;
 	@Autowired
 	ProfilRemunerationRepository repoProfilRemuneration;
+	@Autowired
+	PeriodeRepository repoPeriode;
 
 	@Override
 	public void initialiser() {
@@ -42,7 +49,14 @@ public class InitialiserDonneesServiceDev implements InitialiserDonneesService {
 
 		Map<String, ProfilRemuneration> beansOfTypePro = context.getBeansOfType(ProfilRemuneration.class);
 		beansOfTypePro.values().stream().forEach(p -> repoProfilRemuneration.save(p));
-	}
 
+		IntStream.range(1, 13).forEach(mois -> {
+			Periode p = new Periode();
+			p.setDateDebut(LocalDate.of(LocalDate.now().getYear(), mois, 1));
+			p.setDateFin(p.getDateDebut().with(TemporalAdjusters.lastDayOfMonth()));
+			repoPeriode.save(p);
+		});
+
+	}
 
 }
